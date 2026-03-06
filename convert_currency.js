ef426b606b6f4b2c7ed4f64654516e9a818316a6
xv-kise <salmonsushitaro@gmail.com>
2026-03-07 05:25:33 +0900

    Update convert_currency.js

--- convert_currency.js
+++ convert_currency.js
@@ -1,14 +1,15 @@
 var box1 = document.getElementById("textbox1");
-var alphabet_money = //アルファベットに対応する通貨記号一覧（ここになければそのまま返される）
+var result = document.getElementById("result");
+var alphabet_money = //アルファベットに対応する通貨記号一覧
 {"A":"₳", "B":"฿", "C":"¢", "D":"₫", "E":"€", "F":"₣", "G":"₲", "K":"₭", "L":"₺",
     "M":"₥", "N":"₦", "O":"¤", "P":"₽", "S":"$", "T":"₮", "W":"₩", "Y":"¥"}
 
-function changelabel(next_text){ //ラベルを変える関数（汎用）
-    var text = document.getElementById("display");
+function changelabel(label, next_text){ //未使用
+    let text = document.getElementById(label);
     text.innerHTML = next_text;
 }
-function conv_currency(){ //通貨記号に変換する関数
-    const original = box1.value;
+function conv_currency(text){ //通貨記号に変換する関数
+    const original = text;
     var result = [];
     for (var loop = 0; loop < original.length; loop++) {
         i = original.charAt(loop); //その文字の変換先が入る変数
@@ -22,4 +23,10 @@ function conv_currency(){ //通貨記号に変換する関数
     }
     result = result.join("");
     return result;
+}
+function convert_and_display(){//結果を表示する関数
+    result.innerHTML = "";
+    let resultb = document.createElement("b");
+    resultb.textContent = conv_currency(box1.value);
+    result.appendChild(resultb);
 }
